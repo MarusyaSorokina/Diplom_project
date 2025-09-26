a522f02664ca5f12d1104d7507540474d0300b53
marusya <marina-sorokina-1983@inbox.ru>
2025-09-26 22:47:45 +0300

    Add css and base_home.html

--- project/fithome/coach/models.py
+++ project/fithome/coach/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Coach(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
+    description = models.TextField()
     image = models.ImageField(upload_to='coach/images/')
     program = models.CharField(max_length=400)
-    courses = models.CharField(max_length=400)
+
 
