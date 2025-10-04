ea4c96f5dd32545203bd6f211ca294f59a056394
marusya <marina-sorokina-1983@inbox.ru>
2025-10-05 00:19:21 +0300

    Add style.css and models

--- project/fithome/coach/models.py
+++ project/fithome/coach/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Coach(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='coach/images/')
-    program = models.CharField(max_length=400)
+    demo_link = models.CharField(max_length=2000, blank=True)
+    source_link = models.CharField(max_length=2000, blank=True)
 
+
+    def __str__(self):
+        return self.title
 
